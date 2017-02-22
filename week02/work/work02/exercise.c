2a08ebb6333c8cba79bd367ac4d7edc091b2e2c3
MUHAMMAD ABRAR - 1206277880 <muhammad.abrar@badak.cs.ui.ac.id>
2017-02-23 02:42:16 +0700

    week02 selesai

---
+++ week02/work/work02/exercise.c
@@ -0,0 +1,15 @@
+/*
+* Name: Muhammad Abrar
+* NPM: 1206277880
+* Class: OS-B
+* Comment: kodingan telah di eksekusi
+*/
+#define LOOP 5
+#include <stdio.h>
+void main() {
+int ii;
+int input = 8; 
+for (ii = 8; ii>LOOP; ii--){
+	printf("%d times %d equals %d\n",input,ii,input * ii);
+}
+}
